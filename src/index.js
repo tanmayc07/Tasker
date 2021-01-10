86f3cf9fe3b3c1bc77086d310b3d3aa80ffacad4
tanmayc07 <tanmayc32@gmail.com>
2021-01-10 19:17:07 +0530

    :heavy_plus_sign: Setup Chakra UI

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// Chakra UI setup
+import { ChakraProvider } from "@chakra-ui/react";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
