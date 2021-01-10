32b4ebce6546cd7fbb18bd74f80b25e996d450e1
tanmayc07 <tanmayc32@gmail.com>
2021-01-10 19:38:38 +0530

    :hammer: Create Header component

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import { Center, Heading } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <Center
+      bg="#77eaff"
+      borderRadius="15px"
+      w="80%"
+      p={4}
+      my={4}
+      mx="auto"
+      color="white"
+    >
+      <Heading as="h1" size="4xl" isTruncated>
+        React Todo
+      </Heading>
+    </Center>
+  );
+};
+
+export default Header;
