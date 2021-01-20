4bfde81bef7bf6fb04dc6cbb3682fd3f2af4183a
tanmayc07 <tanmayc32@gmail.com>
2021-01-20 18:36:34 +0530

    :lipstick: Add success toasts for CRUD events

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import Form from "./Form";
 import TodoList from "./TodoList";
 import { useState } from "react";
 import { v1 as v1id } from "uuid";
+// import Footer from "./Footer";
+import { Box, useToast } from "@chakra-ui/react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const toast = useToast();
 
   const handleCreate = (value) => {
     setTasks([...tasks, { id: v1id(), value }]);
+    toast({
+      position: "bottom-left",
+      title: "Success",
+      description: "Task Created!",
+      status: "success",
+      duration: 9000,
+      isClosable: true,
+    });
   };
 
   const handleDelete = (id) => {
@@ -17,18 +28,35 @@ function App() {
         return task.id !== id;
       })
     );
+    toast({
+      position: "bottom-left",
+      title: "Success",
+      description: "Task Deleted!",
+      status: "success",
+      duration: 9000,
+      isClosable: true,
+    });
   };
 
   const handleDeleteAll = () => {
     setTasks([]);
+    toast({
+      position: "bottom-left",
+      title: "Success",
+      description: "Tasks Deleted!",
+      status: "success",
+      duration: 9000,
+      isClosable: true,
+    });
   };
 
   return (
-    <div className="App">
+    <Box h="100%" className="App">
       <Header />
       <Form handleCreate={handleCreate} handleDeleteAll={handleDeleteAll} />
       <TodoList tasks={tasks} handleDelete={handleDelete} />
-    </div>
+      {/* <Footer /> */}
+    </Box>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box pos="absolute" bottom="0">
+      <h2>
+        Copyright © 2021 | Created by{" "}
+        <a href="http://github.com/tanmayc07">@tanmay</a>
+      </h2>
+    </Box>
+  );
+};
+
+export default Footer;
